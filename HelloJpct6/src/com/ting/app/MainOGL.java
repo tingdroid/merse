4fd66314c21382cfe172f3d8bbee822a54d5f845
tingtronics <olegyk@tingtronics.com>
2013-03-19 05:35:15 -0400

    HelloJpct6 initial - load OBJ models

---
+++ HelloJpct6/src/com/ting/app/MainOGL.java
@@ -0,0 +1,46 @@
+package com.ting.app;
+
+import org.lwjgl.opengl.Display;
+
+import com.threed.jpct.FrameBuffer;
+import com.threed.jpct.IRenderer;
+import com.ting.scene.Scene;
+
+/**
+ * A simple Scene using the OpenGL-renderer.
+ * @author EgonOlsen
+ *
+ */
+public class MainOGL {
+
+	private FrameBuffer buffer;
+	private Scene scene;
+
+	public static void main(String[] args) throws Exception {
+		if (args.length > 0) System.out.println(args[0]);
+		new MainOGL().loop();
+	}
+
+	public MainOGL() throws Exception {
+		scene = new Scene();
+	}
+
+	private void loop() throws Exception {
+		buffer = new FrameBuffer(800, 600, FrameBuffer.SAMPLINGMODE_NORMAL);
+		buffer.disableRenderer(IRenderer.RENDERER_SOFTWARE);
+		buffer.enableRenderer(IRenderer.RENDERER_OPENGL);
+
+		while (!Display.isCloseRequested()) {
+			scene.loop();
+			buffer.clear(scene.background);
+			scene.world.renderScene(buffer);
+			scene.world.draw(buffer);
+			buffer.update();
+			buffer.displayGLOnly();
+			Thread.sleep(10);
+		}
+		buffer.disableRenderer(IRenderer.RENDERER_OPENGL);
+		buffer.dispose();
+		System.exit(0);
+	}
+}
