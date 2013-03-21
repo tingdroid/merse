6b799de9384cb4a4fb795d345273aa05f716cea3
tingtronics <olegyk@tingtronics.com>
2013-03-21 11:28:51 -0400

    HelloJpct5 run from jar

--- HelloJpct5/run/run_opengl.bat
+++ HelloJpct5/run/run_opengl.bat
@@ -1,5 +1,5 @@
-set JPCT_LIB=../../jpct/lib
+set JPCT_LIB=../../../jpct/lib
 set JPCT_JNI=-Djava.library.path=%JPCT_LIB%/lwjgl-2.8.0/native/windows
 set JPCT_CP=%JPCT_LIB%/jpct/jpct.jar;%JPCT_LIB%/lwjgl-2.8.0/jar/lwjgl.jar;%JPCT_LIB%/lwjgl-2.8.0/jar/lwjgl_util.jar
 
-java %JPCT_JNI% -Xmx128m -classpath bin;%JPCT_CP% com.ting.app.MainOGL
+java %JPCT_JNI% -Xmx128m -classpath HelloJpct5.jar;%JPCT_CP% com.ting.app.MainOGL
--- HelloJpct5/src/com/ting/scene/BaseScene.java
+++ HelloJpct5/src/com/ting/scene/BaseScene.java
@@ -1,17 +1,20 @@
 package com.ting.scene;
 
+import java.net.URL;
+
 import com.threed.jpct.Texture;
 import com.threed.jpct.TextureManager;
 
 public class BaseScene {
-
+	static URL baseUrl = BaseScene.class.getResource("/assets/");
+	
 	public static String addTexture(String name, int width, int height) {
 		String shortName = name.contains(".") ? 
 				name.substring(0, name.indexOf(".") - 1) : name;
 
 		TextureManager textureManager = TextureManager.getInstance();
 		if (!textureManager.containsTexture(shortName)) {
-			Texture texture = new Texture(name);
+			Texture texture = new Texture(baseUrl, name);
 			if (width != 0 && height != 0) {
 				// bitmap = BitmapHelper.rescale(bitmap, width, height);
 			}
